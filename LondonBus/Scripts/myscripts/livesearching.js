cf8276d6f05fdfe0556e3e39c4457978c01eaa9c
evje <evgeni.jelamanav@gmail.com>
2016-08-30 20:43:26 +0300

    update 2

--- LondonBus/Scripts/myscripts/livesearching.js
+++ LondonBus/Scripts/myscripts/livesearching.js
@@ -1,6 +1,10 @@
-﻿$(function livesearching() {
+﻿//this script is adding jQuery autocomplete plugin
+$(function livesearching() {
     $("[data-autocomplete-source]").each(function livesearching() {
         var target = $(this);
-        target.autocomplete({ source: target.attr("data-autocomplete-source") });
+        target.autocomplete({
+            source: target.attr("data-autocomplete-source"),
+            delay: 50
+        });
     });
 });
